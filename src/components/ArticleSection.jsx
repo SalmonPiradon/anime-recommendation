c6632f2cb39ce02aafaa623ba5896b98eccd0265
Piradon <piradonleungamornnara@gmail.com>
2026-07-05 13:42:39 +0700

    feat(landing): integrate axios for API calls in ArticleSection and add loading state
    - Added axios as a dependency for making API requests to fetch blog posts.
    - Implemented loading state to enhance user experience during data fetching.
    - Refactored ArticleSection to handle category filtering and pagination for blog posts.
    - Introduced LoadingState component to display a loading indicator while fetching data.

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -1,39 +1,93 @@
-import { BlogCard } from "./ui/BlogCard"
-import { Field, FieldLabel } from "./ui/field"
+import { BlogCard } from "./ui/BlogCard";
+import { Field, FieldLabel } from "./ui/field";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "./ui/select"
+} from "./ui/select";
 
-import { SearchBar } from "./SearchBar"
-import blogPosts from "../data/blogPosts"
-import { useState } from "react"
-
-
-
+import { SearchBar } from "./SearchBar";
+import { LoadingState } from "./LoadingState";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export function ArticleSection() {
-
-  const categories = ["All", "Anime", "Movie", "Series"];
-  const [selectedCategory, setSelectedCategory] = useState("All");
+  const categories = ["Highlight", "Cat", "Inspiration", "General"];
+  const [selectedCategory, setSelectedCategory] = useState("Highlight");
   const [searchQuery, setSearchQuery] = useState("");
+  const [blogPosts, setBlogPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // กรองตาม category ที่เลือก กรองต่อด้วยคำค้นหาใน title หรือ description // filter 2 รอบ
-  const postsByCategory =
-    selectedCategory === "All"
-      ? blogPosts
-      : blogPosts.filter((post) => post.category === selectedCategory);
+  // category รับข้อมูลมาจาก getPostsData(selectedCategory)
+  // filter category จาก select ตรงนี้
+  const fetchPosts = async (category, pageNumber, append = false) => {
+    setIsLoading(true);
 
-  const filteredPosts = postsByCategory.filter((post) => {
+    try {
+      const params = {
+        page: pageNumber,
+        limit: 6,
+      };
+
+      // Highlight แสดงทุก category
+      if (category !== "Highlight") {
+        params.category = category;
+      }
+
+      const response = await axios.get(
+        "https://blog-post-project-api.vercel.app/posts",
+        { params },
+      );
+
+      // แยกการแสดงผลของหน้าแรก กับ หน้าถัดไปเพราะเจอปัญหาการแสดงผลของหน้าแรก
+      if (append) {
+        setBlogPosts((prevPosts) => [...prevPosts, ...response.data.posts]);
+      } else {
+        setBlogPosts(response.data.posts);
+      }
+
+      // ตรวจสอบว่ามีข้อมูลต่อไหม ถ้าไม่มีข้อมูลต่อไปจะไม่แสดงปุ่ม View more
+      setHasMore(response.data.currentPage < response.data.totalPages);
+
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      setHasMore(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setPage(1);
+    setHasMore(true);
+    fetchPosts(selectedCategory, 1, false);
+  }, [selectedCategory]);
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+    fetchPosts(selectedCategory, page + 1, true);
+  };
+
+  // function แปลง date จาก "2024-09-11T00:00:00.000Z" เป็น "11 September 2024"
+  const formatDate = (date) =>
+    new Date(date).toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+
+  // filter ของ serach bar
+  const filteredPosts = blogPosts.filter((post) => {
+    const keyword = searchQuery.toLowerCase();
     return (
-      post.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      post.description.toLowerCase().includes(searchQuery.toLowerCase())
+      post.title.toLowerCase().includes(keyword) ||
+      post.description.toLowerCase().includes(keyword)
     );
   });
-
 
   return (
     <section className="mx-auto flex w-full max-w-[1400px] flex-col gap-10 px-4 py-10">
@@ -62,7 +116,7 @@ export function ArticleSection() {
         <div className="hidden lg:block">
           <SearchBar value={searchQuery} onChange={setSearchQuery} />
         </div>
-        
+
         {/* category filter bar of mobile*/}
         <div className="flex w-full flex-col gap-4 lg:hidden">
           <SearchBar value={searchQuery} onChange={setSearchQuery} />
@@ -71,13 +125,18 @@ export function ArticleSection() {
             <FieldLabel className="text-[16px] font-medium text-stone-500">
               Category
             </FieldLabel>
-            <Select value={selectedCategory} onValueChange={setSelectedCategory}>
+            <Select
+              value={selectedCategory}
+              onValueChange={setSelectedCategory}
+            >
               <SelectTrigger className="h-[50px]! w-full bg-white text-[16px] font-medium text-[#75716B]">
                 <SelectValue placeholder="Select category" />
               </SelectTrigger>
               <SelectContent className="text-[16px] font-medium text-[#75716B]">
                 {categories.map((category) => (
-                  <SelectItem key={category} value={category}>{category}</SelectItem>
+                  <SelectItem key={category} value={category}>
+                    {category}
+                  </SelectItem>
                 ))}
               </SelectContent>
             </Select>
@@ -85,19 +144,35 @@ export function ArticleSection() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 gap-16 md:gap-6 md:grid-cols-2">
-        {filteredPosts.map((post) => (
-          <BlogCard
-            key={post.id}
-            image={post.image}
-            category={post.category}
-            title={post.title}
-            description={post.description}
-            author={post.author}
-            date={post.date}
-          />
-        ))}
-      </div>
+      {isLoading && blogPosts.length === 0 ? (
+        <LoadingState />
+      ) : (
+        <div className="grid grid-cols-1 gap-16 md:gap-6 md:grid-cols-2">
+          {filteredPosts.map((post) => (
+            <BlogCard
+              key={post.id}
+              image={post.image}
+              category={post.category}
+              title={post.title}
+              description={post.description}
+              author={post.author}
+              date={formatDate(post.date)}
+            />
+          ))}
+        </div>
+      )}
+
+      {isLoading && blogPosts.length > 0 && <LoadingState />}
+
+      {hasMore && !(isLoading && blogPosts.length === 0) && (
+        <button
+          type="button"
+          className="mt-10 mb-30 cursor-pointer self-center text-[16px] font-medium text-[#26231E] underline hover:text-[#26231E]/80"
+          onClick={handleLoadMore}
+        >
+          {isLoading ? "Loading..." : "View more"}
+        </button>
+      )}
     </section>
   );
 }
